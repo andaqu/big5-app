b58f53a8f2b219174f9a0ae9fb6a99dfbd68ecdb
wendru18 <wendru18@gmail.com>
2021-04-22 00:27:49 +0200

    attempt test

--- personality_recogniser/wekapy/helper.py
+++ personality_recogniser/wekapy/helper.py
@@ -11,6 +11,7 @@ def run_process(options):
     process = subprocess.Popen(options, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process_output, process_error = map(decode_data, process.communicate())
     if any(word.lower() in process_error for word in ["exception", "error"]):
+        print(process_error)
         for line in process_error.split("\n"):
             if any(word.lower() in line for word in ["exception", "error"]):
                 raise WekaPyException(line.split(' ', 1)[1])
